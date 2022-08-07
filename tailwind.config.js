be65c201478becfefde5a189e0813acab9f9d40a
ByJosephDev <byjosephdev@gmail.com>
2022-08-06 22:14:23 -0500

    release 1.0 - add navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,17 @@ module.exports = {
     extend: {
       fontFamily:{
         'sans': "'Work Sans', sans-serif",
-        'cascadia' : "'CascadiaCode'"
+        'cascadia' : "'CascadiaCode', sans-serif",
+        'inter' : "'Inter', sans-serif"
       },
       colors:{
-        'do-blue-dark' : '#080c2d',
+        'do-blue-dark' : 'rgb(8, 27, 75)',
+        'do-blue-medium-dark' : '#1d274c',
         'do-blue-medium' : 'rgb(20, 86, 255)',
         'do-blue-light' : 'rgb(0, 105, 255)',
+      },
+      container:{
+        padding: '1.4rem',
       }
     },
   },
